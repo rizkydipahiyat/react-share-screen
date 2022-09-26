b45ce6fecc77c80d38ba2b3604025354027faa15
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2022-09-26 12:58:57 +0700

    finsih

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import "./App.css";
 
+const data = [];
+let stream;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const videoRef = useRef();
+	const shareScreen = async () => {
+		if (navigator.mediaDevices.getDisplayMedia) {
+			stream = await navigator.mediaDevices.getDisplayMedia({
+				audio: true,
+				video: {
+					cursor: "always",
+				},
+			});
+			const mr = new MediaRecorder(stream);
+			mr.ondataavailable = (chnk) => {
+				console.log(chnk.data);
+				data.push(chnk.data);
+			};
+			mr.start(1000);
+			mr.onstop = (e) => {
+				console.log(e);
+				const videoUrl = URL.createObjectURL(
+					new Blob(data, { type: "video/mp4" })
+				);
+				alert(videoUrl);
+				videoRef.current.src = videoUrl;
+			};
+			videoRef.current.srcObject = stream;
+		}
+	};
+	return (
+		<div className="App">
+			<button onClick={() => shareScreen()}>Playing</button>
+			<button
+				onClick={() => {
+					let tracks = videoRef.current.srcObject.getTracks();
+					tracks.forEach((track) => track.stop());
+					videoRef.current.srcObject = null;
+				}}
+			>
+				Play
+			</button>
+			<header className="App-header">
+				<video width={800} height={800} ref={videoRef} autoPlay controls />
+			</header>
+		</div>
+	);
 }
 
 export default App;
